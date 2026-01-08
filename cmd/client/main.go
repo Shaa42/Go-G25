a12eac1d2e148b19f8a107414032dc21da6ef803
taigamats <taigamatsumoto45@gmail.com>
2026-01-08 11:18:28 +0100

    YOO

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("This is the main code for the client")
+
+}
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:6666")
+	conn, err := listener.Accept()
+	data := make([]byte, 1024)
+	n, err := conn.Read(data)
+	fmt.Println("Received:", string(buffer[:n]))
+
+	conn.Write([]byte("Hello from server"))
 }
